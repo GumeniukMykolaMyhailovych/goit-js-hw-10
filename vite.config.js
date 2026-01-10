c2685052afed870a32b57e99932e3b34c2671f9e
Mykola Gumeniuk <gumeniuk.mykola@gmail.com>
2026-01-10 19:29:14 +0200

    HwJs - 10

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import SortCss from 'postcss-sort-media-queries';
 export default defineConfig({
   root: 'src',
   
-  base: '/goit-js-hw-10/',
+  base: './',
   
   build: {
     outDir: '../dist',
